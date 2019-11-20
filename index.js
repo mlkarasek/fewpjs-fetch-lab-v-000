0c8ece344e93fe3df023a5f3fe0d5bcbcb23b385
Monica  Karasek <mlkarasek@gmail.com>
2019-11-20 22:15:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  return fetch('https://anapioficeandfire.com/api/books');
 }
 
 function renderBooks(json) {
